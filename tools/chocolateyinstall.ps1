9c44aee734e910e7faa58dde2d365481b49d3d02
Félix Piédallu <felix@piedallu.me>
2019-04-18 12:07:40 +0200

    add autohotkey code.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,4 +1,3 @@
-﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
@@ -12,5 +11,14 @@ $packageArgs = @{
   checksum      = '2b6ac53d72b33f01a61d95e6257a8da567c3a3fceef50a82ecad948c5af94895'
   fileType      = 'exe'
 }
+
+#Thanks to dtgm and the GitHub package for ideas.
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "bépoInstall.ahk"
+$ahkProc = Start-Process -FilePath $ahkExe -ArgumentList $ahkFile -PassThru
+
+$ahkId = $ahkProc.Id
+Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$ahkId"
 
 Install-ChocolateyPackage @packageArgs
